5b6c695e9d0717b9a79e65f5ef985a41c1cf636b
Ana Pilar Barba López <pilarbarba03@gmail.com>
2023-01-20 10:05:45 +0100

    Implementando la clase genérica

--- 12_RestQuery/src/main/java/com/salesianostriana/dam/restquery/search/spec/GenericSpecification.java
+++ 12_RestQuery/src/main/java/com/salesianostriana/dam/restquery/search/spec/GenericSpecification.java
@@ -1,5 +1,6 @@
 package com.salesianostriana.dam.restquery.search.spec;
 
+import com.salesianostriana.dam.restquery.search.util.SearchCriteria;
 import org.springframework.data.jpa.domain.Specification;
 
 import javax.persistence.criteria.CriteriaBuilder;
@@ -8,6 +9,9 @@ import javax.persistence.criteria.Predicate;
 import javax.persistence.criteria.Root;
 
 public class GenericSpecification<T> implements Specification<T> {
+    public GenericSpecification(SearchCriteria searchCriteria) {
+    }
+
     @Override
     public Predicate toPredicate(Root<T> root, CriteriaQuery<?> query,
                                  CriteriaBuilder criteriaBuilder) {
--- 12_RestQuery/src/main/java/com/salesianostriana/dam/restquery/search/spec/GenericSpecificationBuilder.java
+++ 12_RestQuery/src/main/java/com/salesianostriana/dam/restquery/search/spec/GenericSpecificationBuilder.java
@@ -1,4 +1,33 @@
 package com.salesianostriana.dam.restquery.search.spec;
 
+import com.salesianostriana.dam.restquery.search.util.SearchCriteria;
+import org.springframework.data.jpa.domain.Specification;
+
+import java.util.List;
+
 public class GenericSpecificationBuilder<T> {
+
+    private final List<SearchCriteria> params;
+
+    public GenericSpecificationBuilder(List<SearchCriteria> params){
+
+        this.params = params;
+    }
+
+    public Specification<T> build(){
+
+        if (params.isEmpty()){
+
+            return null;
+        }
+
+        Specification<T> result = new GenericSpecification<T>(params.get(0));
+
+        for (int i = 1; i < params.size(); i++){
+
+            result = result.and(new GenericSpecification<T>(params.get(i)));
+        }
+
+        return result;
+    }
 }
